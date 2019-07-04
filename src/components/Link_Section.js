ff941051b5db4953c093f00ddf714e4d0bc21955
Shihabudheen US <shihabudheen@bounceshare.com>
2019-07-05 03:25:26 +0530

    fix: Linking

--- src/components/Link_Section.js
+++ src/components/Link_Section.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet,Linking } from "react-native";
 import PropTypes from "prop-types";
 
 export const LinkSection = ({ source,url }) => {
